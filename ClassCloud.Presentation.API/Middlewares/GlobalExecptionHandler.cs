cddd1830e9260b538aca64e0716958bd36cd115c
Maria Magnell <maria.magnell@gmail.com>
2026-02-10 16:12:21 +0100

    added endpoints to courses.

---
+++ ClassCloud.Presentation.API/Middlewares/GlobalExecptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassCloud.Presentation.API.Middlewares;
+
+
+public class GlobalExecptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = exception switch
+        {
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return await httpContext.RequestServices
+            .GetRequiredService<IProblemDetailsService>()
+            .TryWriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = httpContext,
+                ProblemDetails = new ProblemDetails
+                {
+                    Title = "Server Error",
+                    Detail = "An unexpected error occured. Please try again"
+                }
+            });
+    }
+}
