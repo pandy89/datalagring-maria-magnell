aeaa3c3b3925ff7bdb3c09ad0d6d6d4d3f91be82
Maria Magnell <maria.magnell@gmail.com>
2026-02-10 11:32:39 +0100

    BaseRepositoy and IBaseRepositoy

---
+++ ClassCloud.Application/Abstractions/Persistence/IBaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+
+namespace ClassCloud.Application.Abstractions.Persistence;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    //Create
+    Task<TEntity> CreateAsync(TEntity entity, CancellationToken ct = default);
+
+    // Exisits
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> findBy);
+
+    // Get One
+    Task<TEntity?> GetOneAsync(Expression<Func<TEntity,bool>> where, CancellationToken ct = default);
+
+    // Get whole entity, good for write/update, Less flexible
+    Task<IReadOnlyList<TEntity>> GetAllAsync(
+        Expression<Func<TEntity, bool>>? where = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        bool tracking = false,
+        CancellationToken ct = default,
+        params Expression<Func<TEntity, object>>[] includes);
+
+    // Get specific entites information, only selected fields, Perfect for reading, More flexible & efficient
+    Task<IReadOnlyList<TSelect>> GetAllAsync<TSelect>(
+        Expression<Func<TEntity, TSelect>> select,
+        Expression<Func<TEntity, bool>>? where = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        bool tracking = false,
+        CancellationToken ct = default,
+        params Expression<Func<TEntity, object>>[] includes);
+
+    // Update
+    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken ct = default);
+
+    // Delete
+    Task DeleteAsync(TEntity entity, CancellationToken ct = default);
+
+    // Save
+    Task<int> SaveChangesAsync(CancellationToken ct = default);
+}
