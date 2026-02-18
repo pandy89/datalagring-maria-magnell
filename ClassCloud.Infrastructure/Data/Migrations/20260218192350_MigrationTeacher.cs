825b7a9f002b8cd0f04380e5d418572e8266f306
Maria Magnell <maria.magnell@gmail.com>
2026-02-18 20:25:55 +0100

    miragration to teachertabels

---
+++ ClassCloud.Infrastructure/Data/Migrations/20260218192350_MigrationTeacher.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassCloud.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationTeacher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Teachers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Teachers_Email",
+                table: "Teachers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "UQ_Teachers_Email_NotEmpty",
+                table: "Teachers",
+                sql: "LTRIM(RTRIM([Email])) <> ''");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ_Teachers_Email",
+                table: "Teachers");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "UQ_Teachers_Email_NotEmpty",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Teachers");
+        }
+    }
+}
